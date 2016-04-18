a1bbba8f71ff6997d901ae54597a4d9b747cd26b
Lawrence Tran <lawrence@tran.com>
2016-04-18 00:01:25 -0500

    Text view adjusts for line number

--- SubtitleView/Classes/SubtitleView.swift
+++ SubtitleView/Classes/SubtitleView.swift
@@ -35,7 +35,7 @@ public class SubtitleView: UIView {
     
     private func setupTextView() {
         self.textView = UITextView(frame: CGRect(x: 0,
-            y: frame.height * 0.75,
+            y: frame.height * 0.85,
             width: frame.width,
             height: frame.height * 0.25))
         self.initialFontSize = dynamicFontSize()
@@ -59,26 +59,8 @@ public class SubtitleView: UIView {
     }
     
     private func dynamicFontSize() -> CGFloat {
-        let normalizer = CGFloat(240.0 / 20.0)
-//        let fontScale = CGFloat(20.0)
-        let scaleFactor = self.frame.width / normalizer
-        return scaleFactor
-    }
-}
-
-extension SubtitleView: UITextViewDelegate {
-    
-    public func textViewDidChange(textView: UITextView) {
-        let string = NSAttributedString(string: textView.text, attributes: [
-            NSStrokeColorAttributeName : UIColor.blackColor(),
-            NSForegroundColorAttributeName : UIColor.whiteColor(),
-            NSStrokeWidthAttributeName : NSNumber(float: -2.0),
-            NSFontAttributeName : UIFont(name: "Arial-BoldItalicMT", size: self.fontSize)!
-            ])
-        self.textView.attributedText =  string
-        self.textView.textAlignment = .Center
-        
-        print(string.size().height)
+        let scaleFactor = CGFloat(240.0 / 22.0)
+        return self.frame.width / scaleFactor
     }
     
     private func sizeOfString (string: String, constrainedToWidth width: Double, font: UIFont) -> CGSize {
@@ -87,6 +69,22 @@ extension SubtitleView: UITextViewDelegate {
             attributes: [NSFontAttributeName: font],
             context: nil).size
     }
+}
+
+extension SubtitleView: UITextViewDelegate {
+    
+    public func textViewDidChange(textView: UITextView) {
+        if (textView.text != "") {
+            let string = NSAttributedString(string: textView.text, attributes: [
+                NSStrokeColorAttributeName : UIColor.blackColor(),
+                NSForegroundColorAttributeName : UIColor.whiteColor(),
+                NSStrokeWidthAttributeName : NSNumber(float: -2.0),
+                NSFontAttributeName : UIFont(name: "Arial-BoldItalicMT", size: self.fontSize)!
+                ])
+            textView.attributedText =  string
+            textView.textAlignment = .Center
+        }
+    }
     
     public func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
         let newText = (textView.text as NSString).stringByReplacingCharactersInRange(range, withString: text)
@@ -94,7 +92,17 @@ extension SubtitleView: UITextViewDelegate {
         textWidth -= 2.0 * textView.textContainer.lineFragmentPadding;
         let boundingRect = sizeOfString(newText, constrainedToWidth: Double(textWidth), font: textView.font!)
         let numberOfLines = boundingRect.height / textView.font!.lineHeight;
-        return numberOfLines <= 2;
+        
+        if (numberOfLines <= 2) {
+            if (numberOfLines == 1) {
+                textView.frame = CGRect(x: 0, y: frame.height * 0.85, width: frame.width, height: frame.height * 0.15)
+            } else if (numberOfLines == 2){
+                textView.frame = CGRect(x: 0, y: frame.height * 0.75, width: frame.width, height: frame.height * 0.25)
+            }
+            return true
+        } else {
+            return false
+        }
     }
 }
 
